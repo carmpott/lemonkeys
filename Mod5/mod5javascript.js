9fb4353f0216a4d37f4580ce73f78787e541c68c
Carmela Potter <129919763+carmpott@users.noreply.github.com>
2023-05-06 18:05:55 -0700

    Update mod5javascript.js

--- Mod5/mod5javascript.js
+++ Mod5/mod5javascript.js
@@ -3,18 +3,16 @@ var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
 
+// Ask the user for a number from 0-10  (In this assignment, you are not required to validate the input but you can if you want)
+let table = prompt("Please enter a number 1 - 10");
+
+// You will display the multiplication table based on the number entered by the user
 if (operator === 'multipication') {
   // Do addition
-  while (i < 10) {
+  while (i =< table) {
     msg += i + ' x 5 = ' + (i + table) + '<br />';
     i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+    }
 }
 
 // Write the message into the page
