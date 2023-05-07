7e1440f81d973fb9332ceaeaac8e58d7c2db4a9a
Carmela Potter <129919763+carmpott@users.noreply.github.com>
2023-05-06 18:44:56 -0700

    Update mod5javascript.js
    fixed

--- Mod5/mod5javascript.js
+++ Mod5/mod5javascript.js
@@ -1,19 +1,21 @@
-var table;             // Unit of table
-var operator = 'multiplication'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var table; // Unit of table
+// var operator = 'addition'; // Type of calculation
+var i = 1; // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>'; // Message
 
-// Ask the user for a number from 0-10  (In this assignment, you are not required to validate the input but you can if you want)
-table = prompt("Please enter a number 1 - 10");
-
-// You will display the multiplication table based on the number entered by the user
-if (operator === 'multipication') {
+/* if (operator === 'addition') {
   // Do addition
-  while (i =< table) {
-    msg += i + ' x 5 = ' + (i + table) + '<br />';
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
-    }
-}
+  }
+} else { */
+  // Do multiplication
+table = Number(prompt('Enter a number 1 - 10'));
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
