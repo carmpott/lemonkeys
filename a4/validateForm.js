73ab10797c78597361102a8b8701fcc20824a3b7
Carmela Potter <129919763+carmpott@users.noreply.github.com>
2023-05-27 18:58:22 -0700

    Update validateForm.js
    i dont know what im doing

--- a4/validateForm.js
+++ a4/validateForm.js
@@ -14,3 +14,33 @@ var frmvalidator = new Validator("myform");
   frmvalidator.addValidation("Address","maxlen=50"); 
   frmvalidator.addValidation("Country","dontselect=000"); 
 
+function validate (){
+//1) Create variable
+
+var validFirstname=false;
+
+//2) read value from HTML
+
+var firstname = document.getElementById('firstname').value;
+
+//3) Do validation
+
+if (firstname === '' || firstname === "null" || firstname.length > 20){
+
+    errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
+
+}
+else
+   validFirstname = true;
+
+//4) Send error message to HTML
+
+document.getElementById("errorMessages").innerHTML = errorMessages;
+
+//5) return status of each field
+
+return (validFirstname);
+
+
+
+}
