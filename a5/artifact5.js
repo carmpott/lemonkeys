2f607f967a7258fc72f71105f18f5144409fbb97
Carmela Potter <129919763+carmpott@users.noreply.github.com>
2023-06-02 21:03:21 -0700

    Update artifact5.js
    4 seconds

--- a5/artifact5.js
+++ a5/artifact5.js
@@ -10,7 +10,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 4000);
 }
 
 function plusSlides(position) {
