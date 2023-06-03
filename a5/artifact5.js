3d389efe349029a1426d404c85089377a40c93c6
Carmela Potter <129919763+carmpott@users.noreply.github.com>
2023-06-02 20:57:37 -0700

    Update artifact5.js

--- a5/artifact5.js
+++ a5/artifact5.js
@@ -10,6 +10,18 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000);
 }
 
+function plusSlides(position) {
+  slideIndex += position;
+  if (slideIndex>slides.length) 
+  {slideIndex = 1}
+  
+  else if(slideIndex<1) 
+  {slideIndex = slides.length}
+    for (i=0; i<slides.length; i++) {
+       slides[i].style.display = "none";}
+  slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
